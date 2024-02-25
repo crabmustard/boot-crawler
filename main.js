94554b6ee1ddd8d0154052236e36cd9ec591ab95
crabmustard <bryce.lippert@gmail.com>
2024-02-25 07:19:00 -0500

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+const { crawlPage } = require('./crawl.js')
+
+function main() {
+    for (const arg of process.argv) {
+        console.log(arg)
+    }
+    if (process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1)
+    } else if (process.argv.length > 3) {
+        console.log("too much website provided")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+    console.log(`starting crawl of: ${baseURL}`)
+    crawlPage(baseURL)
+
+}
+
+
+main()
