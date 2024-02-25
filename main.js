c243405589b8800c5f433e06bb3fcde70579e7a7
crabmustard <bryce.lippert@gmail.com>
2024-02-25 08:18:33 -0500

    added printReport function and tests

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { crawlPage } = require('./crawl.js')
+const { printReport } = require('./report.js')
 
 async function main() {
     for (const arg of process.argv) {
@@ -14,10 +15,7 @@ async function main() {
     const baseURL = process.argv[2]
     console.log(`starting crawl of: ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
-    
-    for (const page of Object.entries(pages)) {
-        console.log(page)
-    }
+    printReport(pages)
 }
 
 
